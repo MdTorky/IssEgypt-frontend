6258a6271ecee8eea2b528296f973b72eef3a643
cargo <mohamed2003torky@gmail.com>
2023-11-27 14:01:22 +0800

    Image 4

--- src/Components/Form/MemberForm.js
+++ src/Components/Form/MemberForm.js
@@ -309,7 +309,7 @@ const MemberForm = ({ language, languageData, api, darkMode }) => {
 
                     {img && (
                         <div className="InputField">
-                            <img src={`/uploads/${img.filename}`} alt="Uploaded Image" />
+                            <img src={`/images/${img.filename}`} alt="Uploaded Image" />
                         </div>
                     )}
                     <button>{languageText.addMember}</button>
