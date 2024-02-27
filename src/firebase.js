ce66b7ea8044324fb1783eefa4661bdc7a8aced6
cargo <mohamed2003torky@gmail.com>
2024-02-27 20:55:23 +0200

    Testing More Security

--- src/firebase.js
+++ src/firebase.js
@@ -12,9 +12,9 @@ const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
     projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
+    storageBucket: "iss-egypt-files.appspot.com",
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    appId: process.env.FIREBASE_APP_ID,
 };
 
 
