0e096de6d3e9e739dda9bc656d31f0cea1b86344
cargo <mohamed2003torky@gmail.com>
2024-02-27 20:36:35 +0200

    Testing More Security

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCoTkKZ2jWdHCR10hpQWNvjN7WLVCJWpzk",
-    authDomain: "iss-egypt-files.firebaseapp.com",
-    projectId: "iss-egypt-files",
-    storageBucket: "iss-egypt-files.appspot.com",
-    messagingSenderId: "1062478836402",
-    appId: "1:1062478836402:web:5572eb633d88eebf1d8fab"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
