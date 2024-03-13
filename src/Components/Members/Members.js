9cd3a3f1ebd7824af797b2843c3f6760ff6c0649
cargo <mohamed2003torky@gmail.com>
2024-03-13 03:44:15 +0200

    Fix

--- src/Components/Members/Members.js
+++ src/Components/Members/Members.js
@@ -241,7 +241,6 @@ const Members = ({ language, languageData, api, darkMode }) => {
                             {membersToShow.map((boardMember) => (
                                 <MemberCard key={boardMember.index} api={api} member={boardMember} languageText={languageText} language={language} />
                             ))}
-                            {/* {loading2(number)} */}
                         </>
                     </div>
                 </div>
